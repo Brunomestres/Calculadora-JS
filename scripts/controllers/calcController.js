a3888f377208c0777b4e4cbda808596f47bea898
Bruno Ribeiro <brunomestres@gmail.com>
2019-05-08 20:05:21 -0300

    Initial

---
+++ scripts/controllers/calcController.js
@@ -0,0 +1,78 @@
+class calcController{
+
+    constructor()
+    {
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._location = "pt-BR";
+        
+        this._currentDate;
+        this.initialize();
+    }
+
+
+    initialize()
+    {
+        this.setDisplayDateTime();
+        
+        setInterval(()=>{
+            this.setDisplayDateTime();
+        },1000);
+        
+        
+    }
+
+    setDisplayDateTime()
+    {
+        this.displayDate = this.currentDate.toLocaleDateString(this._location,{
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._location);
+    }
+
+
+    get displayDate()
+    {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value)
+    {
+        this._dateEl.innerHTML = value;
+    }
+
+    get displayTime()
+    {
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value)
+    {
+        this._timeEl.innerHTML = value;
+    }
+
+
+    get displayCalc()
+    {
+        return _displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(value)
+    {
+        this._displayCalcEl.innerHTML = value;
+    }
+
+
+    get currentDate()
+    {
+        return new Date();
+    }
+
+    set currentDate(value)
+    {
+        this._currentDate = value;
+    }
+}
